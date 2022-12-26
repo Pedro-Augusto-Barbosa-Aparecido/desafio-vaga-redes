464d26b7070b738e8ac0c68de5343921b32efc21
Pedro-Augusto-Barbosa-Aparecido <77130441+Pedro-Augusto-Barbosa-Aparecido@users.noreply.github.com>
2022-12-26 16:47:22 -0300

    chore 🔧 config database

--- backend/src/proj/settings.py
+++ backend/src/proj/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "postgres",
+        "USER": "postgres",
+        "PASSWORD": "postgrespw",
+        "HOST": "localhost",
+        "PORT": 49153,
     }
 }
 
