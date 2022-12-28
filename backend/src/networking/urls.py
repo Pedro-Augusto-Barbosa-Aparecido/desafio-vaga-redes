3d1ffc52046260a626ccb2cf0e3b67c5572e9af3
Pedro-Augusto-Barbosa-Aparecido <77130441+Pedro-Augusto-Barbosa-Aparecido@users.noreply.github.com>
2022-12-28 11:14:22 -0300

    feat ✨adding endpoint to consume info

--- backend/src/networking/urls.py
+++ backend/src/networking/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from .views import NetworkingView
+from .views import NetworkingConnectOnCellView, NetworkingListInformation
 
 urlpatterns = [
-  path("<str:city>/", NetworkingView.as_view(), name="networking")
+  path("connect/<str:city>/", NetworkingConnectOnCellView.as_view(), name="networking-connect"),
+  path("info/<str:city>/", NetworkingListInformation.as_view(), name="networking-list-information"),
 ]
--- backend/src/networking/views.py
+++ backend/src/networking/views.py
@@ -7,7 +7,7 @@ from django.views.generic import View
 from .models import Networking, NetworkingInformation
 
 
-class NetworkingView(View):
+class NetworkingConnectOnCellView(View):
   """Class to simulate connection on nodeb
   """
   def get(self, request, city: str):
@@ -19,7 +19,7 @@ class NetworkingView(View):
     cell_to_connect = Networking.objects.get(pk=cell_to_connect["pk"])
 
     # size information trafficked simulation
-    size = sys.getsizeof(request) * random.randrange(10**6, 11**6) + random.random()
+    size = sys.getsizeof(request) * random.randrange(10**6, 11**6) * 0.03 + random.random()
 
     # save connection information
     connection_information = \
@@ -33,3 +33,22 @@ class NetworkingView(View):
         "networking_connected": connection_information.networking.enodeb_name
       }
     })
+
+
+class NetworkingListInformation(View):
+  def get(self, request, city: str):
+    cell_connection_information = NetworkingInformation.objects.all().filter(
+      networking__location__name__icontains=city
+    ).values(
+      "created_at", "id", "networking__cell_name", "networking_id", "size"
+    )  
+    
+    # formating returns
+    information_formatted = {}
+    for info in cell_connection_information:
+      if info["networking__cell_name"] not in information_formatted.keys():
+        information_formatted[info["networking__cell_name"]] = [info]
+      else:
+        information_formatted[info["networking__cell_name"]].append(info)
+
+    return JsonResponse({"results": information_formatted})
